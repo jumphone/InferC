b3af9bbfe84ac2999bbc13a9c8b6ce676323936b
Zhang, Feng <fzhang15@fudan.edu.cn>
2024-08-14 13:25:01 +0800

    Delete src/s03_atlas_fetal_adult_COM_scATAC directory

--- src/s03_atlas_fetal_adult_COM_scATAC/step01_getLST.sh
+++
@@ -1,6 +0,0 @@
-ls /home/database/data/scATAC_ATLAS/raw/hg38/*.format -d1 > LST_ADULT.txt
-ls /home/database/data/scATAC_ATLAS_FETAL/raw/hg38/*.format -d1 > LST_FETAL.txt
-
-
-
-
--- src/s03_atlas_fetal_adult_COM_scATAC/step03_combine.sh
+++
@@ -1,2 +0,0 @@
-/home/database/data/ABI_KIMI/abi.py LST_ADULT.txt 有哪些器官或组织
-/home/database/data/ABI_KIMI/abi.py LST_FETAL.txt 有哪些器官或组织
