f4d962f2ebabc99b8be198ddbab9bc0c6fd8708f
Zhang, Feng <fzhang15@fudan.edu.cn>
2024-08-14 12:54:39 +0800

    Delete s01_scATAC_scHIC_frontal_cortex directory

--- s01_scATAC_scHIC_frontal_cortex/step13_processBedpe.sh
+++
@@ -1,6 +0,0 @@
-
-bedtools pairtopair -type both -a ./scHIC/Merged_loop_within500k.bedpe -b  ./rds/ciceroFrame_conns_uniq.bedpe >  ./scHIC/Merged_loop_within500k_overPeakPair_allinfo.bedpe
-
-
-bedtools pairtopair -type both -a ./scHIC/Merged_loop_within500k.bedpe -b  ./rds/ciceroFrame_conns_uniq.bedpe | cut -f 1,2,3,4,5,6 - | uniq -> ./scHIC/Merged_loop_within500k_overPeakPair_uniq.bedpe
-
